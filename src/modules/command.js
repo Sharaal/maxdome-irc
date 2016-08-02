acaf7d37d6487b9f15fbf3b96cbe9a34cec922f8
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-08-02 13:25:57 +0200

    Formatting

--- src/modules/command.js
+++ src/modules/command.js
@@ -8,16 +8,16 @@ module.exports = ({ admin, client, commands, from, message, reply, replyto }) =>
     return;
   }
   const index = message.indexOf(' ');
-  let command;
+  let commandName;
   let args;
   if (index !== -1) {
-    command = message.substring(1, index);
+    commandName = message.substring(1, index);
     args = message.substring(index + 1);
   } else {
-    command = message.substring(1);
+    commandName = message.substring(1);
   }
-  if (!commands[command]) {
+  if (!commands[commandName]) {
     return;
   }
-  return commands[command]({ admin, args, client, from, reply, replyto });
+  return commands[commandName]({ admin, args, client, from, reply, replyto });
 };
