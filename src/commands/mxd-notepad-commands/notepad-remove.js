377364d4b4892bb4eca43cd97e0fa48111e48956
Christoph Herrmann <developer@dragonprojects.de>
2016-09-08 14:32:45 +0200

    Remove assets from notepad

--- src/commands/mxd-notepad-commands/notepad-remove.js
+++ src/commands/mxd-notepad-commands/notepad-remove.js
@@ -1,3 +1,13 @@
-module.exports = ({ heimdall, heimdallSessions }) => async ({ args, reply }) => {
-  reply.send(`Remove "${args}" from the notepad`);
+module.exports = ({ heimdall, heimdallSessions }) => async ({ args, heimdallLoggedin, reply }) => {
+  const { session } = await heimdallLoggedin();
+  try {
+    await heimdall.delete(`mxd/notepad/${session.customer.customerId}/content/${args}`, { 'mxd-session': session.sessionId });
+    reply.send(`remove asset with id "${args}" from the notepad`);
+  } catch (e) {
+    if (e.error) {
+      reply.send(`notepad-remove error "${e.error.message}"`);
+    } else {
+      reply.send(`notepad-remove error "${e.message}"`);
+    }
+  }
 };
