eb9a62d9192efce889314c2485dade05c4d7518c
Christoph Herrmann <developer@dragonprojects.de>
2016-09-08 14:19:34 +0200

    Add asset to notepad

--- src/commands/mxd-notepad-commands/notepad-add.js
+++ src/commands/mxd-notepad-commands/notepad-add.js
@@ -1,3 +1,13 @@
-module.exports = ({ heimdall, heimdallSessions }) => async ({ args, reply }) => {
-  reply.send(`Add "${args}" to the notepad`);
+module.exports = ({ heimdall, heimdallSessions }) => async ({ args, heimdallLoggedin, reply }) => {
+  const { session } = await heimdallLoggedin();
+  try {
+    await heimdall.post(`mxd/notepad/${session.customer.customerId}`, { contentId: args }, { 'mxd-session': session.sessionId });
+    reply.send(`added asset with id "${args}" to the notepad`);
+  } catch (e) {
+    if (e.error) {
+      reply.send(`notepad-add error "${e.error.message}"`);
+    } else {
+      reply.send(`notepad-add error "${e.message}"`);
+    }
+  }
 };
