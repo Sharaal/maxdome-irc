b664edfe921dcafb505d64359b6b676695df7403
Christoph Herrmann <developer@dragonprojects.de>
2016-09-08 14:32:34 +0200

    Add opportunity to send "delete" requests

--- src/modules/heimdall.js
+++ src/modules/heimdall.js
@@ -1,27 +1,33 @@
 const rp = require('request-promise');
 
 module.exports = ({ apikey, appid }) => {
+  const request = async (method, path, body, headers) => {
+    headers = Object.assign(
+      {
+        accept: 'application/json',
+        client: 'mxd_package',
+        clienttype: 'Webportal',
+        'content-type': 'application/json',
+        language: 'de_DE',
+        'maxdome-origin': 'maxdome.de',
+        platform: 'web'
+      },
+      headers
+    );
+    return rp[method]({
+      body,
+      url: `https://heimdall.maxdome.de/api/v1/${path}`,
+      headers,
+      json: true,
+      qs: { apikey, appid }
+    });
+  };
   return {
+    delete: async (path, headers) => {
+      return request('delete', path, undefined, headers);
+    },
     post: async (path, body, headers) => {
-      headers = Object.assign(
-        {
-          accept: 'application/json',
-          client: 'mxd_package',
-          clienttype: 'Webportal',
-          'content-type': 'application/json',
-          language: 'de_DE',
-          'maxdome-origin': 'maxdome.de',
-          platform: 'web'
-        },
-        headers
-      );
-      return rp.post({
-        body,
-        url: `https://heimdall.maxdome.de/api/v1/${path}`,
-        headers,
-        json: true,
-        qs: { apikey, appid }
-      });
+      return request('post', path, body, headers);
     }
   };
 };
