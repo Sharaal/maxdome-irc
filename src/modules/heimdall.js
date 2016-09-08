0d9628063988281205cc47ff61a8903e9b886d87
Christoph Herrmann <developer@dragonprojects.de>
2016-09-08 15:12:04 +0200

    Add opportunity to search for assets

--- src/modules/heimdall.js
+++ src/modules/heimdall.js
@@ -1,7 +1,8 @@
+const AssetsQuery = require('mxd-heimdall').AssetsQuery;
 const rp = require('request-promise');
 
 module.exports = ({ apikey, appid }) => {
-  const request = async (method, path, body, headers) => {
+  const request = async (method, path, { body, headers, transform }) => {
     headers = Object.assign(
       {
         accept: 'application/json',
@@ -14,20 +15,44 @@ module.exports = ({ apikey, appid }) => {
       },
       headers || {}
     );
+    if (path.includes('?')) {
+      path += '&';
+    } else {
+      path += '?';
+    }
+    path += `apikey=${apikey}&appid=${appid}`;
     return rp[method]({
-      body,
+      body: body || {},
       url: `https://heimdall.maxdome.de/api/v1/${path}`,
       headers,
       json: true,
-      qs: { apikey, appid }
+      transform
     });
   };
   return {
     delete: async (path, headers) => {
-      return request('delete', path, undefined, headers);
+      return request('delete', path, { headers });
     },
     post: async (path, body, headers) => {
-      return request('post', path, body, headers);
+      return request('post', path, { body, headers });
+    },
+    getAssets: async search => {
+      const query = (new AssetsQuery())
+        .filter('contentTypeSeriesOrMovies')
+        .filter('search', search);
+      return await request(
+        'get',
+        `mxd/assets?${query}`,
+        {
+          transform: data => data.assetList.map(asset => {
+            let title = asset.title;
+            if (asset['@class'] === 'MultiAssetTvSeriesSeason') {
+              title += ` (Season ${asset.number})`;
+            }
+            return { id: asset.id, title, description: asset.descriptionShort };
+          })
+        }
+      );
     }
   };
 };
