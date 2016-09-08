ceef01bffae4d21f0003156821702b0ddef325d4
Christoph Herrmann <developer@dragonprojects.de>
2016-09-08 15:12:26 +0200

    Use the args as asset title if its not a number

--- src/commands/mxd-notepad-commands/notepad-add.js
+++ src/commands/mxd-notepad-commands/notepad-add.js
@@ -1,6 +1,14 @@
 module.exports = ({ heimdall, heimdallSessions }) => async ({ args, heimdallLoggedin, reply }) => {
   const { session } = await heimdallLoggedin();
   try {
+    if (!Number.isInteger(args)) {
+      const [asset] = await heimdall.getAssets(args);
+      if (!asset) {
+        reply.send(`no asset found with name "${args}"`);
+        return;
+      }
+      args = asset.id;
+    }
     await heimdall.post(`mxd/notepad/${session.customer.customerId}`, { contentId: args }, { 'mxd-session': session.sessionId });
     reply.send(`added asset with id "${args}" to the notepad`);
   } catch (e) {
--- src/commands/mxd-notepad-commands/notepad-remove.js
+++ src/commands/mxd-notepad-commands/notepad-remove.js
@@ -1,6 +1,14 @@
 module.exports = ({ heimdall, heimdallSessions }) => async ({ args, heimdallLoggedin, reply }) => {
   const { session } = await heimdallLoggedin();
   try {
+    if (!Number.isInteger(args)) {
+      const [asset] = await heimdall.getAssets(args);
+      if (!asset) {
+        reply.send(`no asset found with name "${args}"`);
+        return;
+      }
+      args = asset.id;
+    }
     await heimdall.delete(`mxd/notepad/${session.customer.customerId}/content/${args}`, { 'mxd-session': session.sessionId });
     reply.send(`remove asset with id "${args}" from the notepad`);
   } catch (e) {
