377ed59c326f075041d8e51e9ccb22f2eb03d904
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-13 15:09:33 +0200

    Extract the new general usable commands/modules

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ client.addListener('error', message => {
   console.log('error: ', message);
 });
 
-const Heimdall = require('mxd-heimdall').Heimdall;
+const { AssetsQuery, Heimdall } = require('mxd-heimdall');
 const heimdall = new Heimdall({
   apikey: process.env.HEIMDALL_APIKEY,
   appid: process.env.HEIMDALL_APPID
@@ -20,16 +20,20 @@ const sessionStorage = (() => {
   };
 })();
 
+const mxdAuthCommands = require('mxd-auth-commands');
+const mxdNotepadCommands = require('mxd-notepad-commands');
+
 const commands = {
   '!mxd-help': require('./commands/help.js'),
+  '!mxd-info': require('info-command').commands.info,
   '!mxd-join': require('./commands/join.js'),
-  '!mxd-login': require('./commands/mxd-auth-commands/login.js')({ heimdall, sessionStorage }),
-  '!mxd-logout': require('./commands/mxd-auth-commands/logout.js')({ heimdall, sessionStorage }),
-  '!mxd-notepad-add': require('./commands/mxd-notepad-commands/notepad-add.js')({ heimdall }),
-  '!mxd-notepad-remove': require('./commands/mxd-notepad-commands/notepad-remove.js')({ heimdall }),
+  '!mxd-login': mxdAuthCommands.commands['mxd-login-command']({ heimdall, sessionStorage }),
+  '!mxd-logout': mxdAuthCommands.commands['mxd-logout-command']({ heimdall, sessionStorage }),
+  '!mxd-nodepad-add': mxdNotepadCommands.commands['mxd-nodepad-add-command']({ heimdall }),
+  '!mxd-nodepad-remove': mxdNotepadCommands.commands['mxd-nodepad-remove-command']({ heimdall }),
   '!mxd-part': require('./commands/part.js'),
-  '!mxd-info': require('info-command'),
-  '!mxd-search': require('mxd-search-command')({
+  '!mxd-search': require('mxd-search-command').commands['mxd-search']({
+    AssetsQuery: AssetsQuery,
     heimdall: heimdall,
     pageSize: process.env.HEIMDALL_PAGESIZE || 3
   })
@@ -40,11 +44,13 @@ client.addListener('message', async (from, to, message) => {
   const reply = require('./modules/reply.js')({ client, from, replyto });
   const admin = require('./modules/admin.js')(process.env.ADMIN_ACCOUNT)({ client, from, reply });
   const loggedin = require('./modules/loggedin.js')({ client, from, reply });
-  const heimdallLoggedin = require('./modules/heimdallLoggedin.js')({ sessionStorage })({ loggedin, reply });
+  const heimdallLoggedin = mxdAuthCommands.modules['heimdall-loggedin']({ sessionStorage })({ loggedin, reply });
   try {
     const { commandName, args } = require('./modules/commandName.js')({ from, message, replyto });
     if (commandName && commands[commandName]) {
       await commands[commandName]({ admin, args, client, heimdallLoggedin, loggedin, from, reply, replyto });
     }
-  } catch(e) {}
+  } catch(e) {
+    reply.send(`error "${e.message}"`);
+  }
 });
