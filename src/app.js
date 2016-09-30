b0d460fb5ecf72bffb53ce7a99e3cd0e5f2fbaac
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-30 14:56:41 +0200

    Fix duplicate variable names

--- src/app.js
+++ src/app.js
@@ -4,16 +4,16 @@ const heimdall = new Heimdall({
   appid: process.env.HEIMDALL_APPID
 });
 
-const client = require('redis').createClient(process.env.REDIS_URL);
-const channelStorage = require('mxd-channel-storage')({ client });
-const sessionStorage = require('mxd-session-storage')({ client });
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
+const channelStorage = require('mxd-channel-storage')({ redisClient });
+const sessionStorage = require('mxd-session-storage')({ redisClient });
 
 const irc = require('irc');
 const channels = channelStorage.values();
 channels.push(process.env.IRC_CHANNEL);
-const client = new irc.Client(process.env.IRC_HOST, process.env.IRC_NICK, { channels });
+const ircClient = new irc.Client(process.env.IRC_HOST, process.env.IRC_NICK, { channels });
 
-client.addListener('error', message => {
+ircClient.addListener('error', message => {
   console.log('error: ', message);
 });
 
@@ -35,16 +35,16 @@ const commands = {
   })
 };
 
-client.addListener('message', async (from, to, message) => {
-  const replyto = require('./modules/replyto.js')({ client, from, to });
-  const reply = require('./modules/reply.js')({ client, from, replyto });
-  const loggedin = require('./modules/loggedin.js')({ client, from, reply });
-  const admin = require('./modules/admin.js')(process.env.ADMIN_ID)({ client, from, loggedin, reply });
+ircClient.addListener('message', async (from, to, message) => {
+  const replyto = require('./modules/replyto.js')({ client: ircClient, from, to });
+  const reply = require('./modules/reply.js')({ client: ircClient, from, replyto });
+  const loggedin = require('./modules/loggedin.js')({ client: ircClient, from, reply });
+  const admin = require('./modules/admin.js')(process.env.ADMIN_ID)({ client: ircClient, from, loggedin, reply });
   const heimdallLoggedin = mxdAuthCommands.modules['heimdall-loggedin']({ heimdall, sessionStorage })({ loggedin, reply });
   try {
     const { commandName, args } = require('./modules/commandName.js')({ from, message, replyto });
     if (commandName && commands[commandName]) {
-      await commands[commandName]({ admin, args, client, heimdallLoggedin, loggedin, from, reply, replyto });
+      await commands[commandName]({ admin, args, client: ircClient, heimdallLoggedin, loggedin, from, reply, replyto });
     }
   } catch(e) {
     reply.send(`error "${e.message}"`);
